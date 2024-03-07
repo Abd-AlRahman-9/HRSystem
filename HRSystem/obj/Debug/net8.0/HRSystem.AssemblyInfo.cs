4fa8dc1d0b6af014da8f103e1b561236231d03f3
Abd-AlRahman-9 <eldesoqe@outlook.com>
2024-03-07 14:26:46 +0200

    After Refactoring Thursday in the Institution

--- HRSystem/obj/Debug/net8.0/HRSystem.AssemblyInfo.cs
+++ HRSystem/obj/Debug/net8.0/HRSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4aeccef520b0ffa814671b9b92f653fa19b157b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a694c65bce676ceef0f7634a0a9a300aa337b93e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
