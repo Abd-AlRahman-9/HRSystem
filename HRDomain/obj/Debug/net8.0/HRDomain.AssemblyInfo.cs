633135ace7adb578a5da30176d4decd11390d0c9
Aya Hamad <aya408757@gmail.com>
2024-03-18 16:55:15 +0200

    remove middelware

--- HRDomain/obj/Debug/net8.0/HRDomain.AssemblyInfo.cs
+++ HRDomain/obj/Debug/net8.0/HRDomain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e5f24e7f2a7230e35ea94507ce6dd18a5bda43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d830410e4377c2196672bb29d7495cbacb4b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
