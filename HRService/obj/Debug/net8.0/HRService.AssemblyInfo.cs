dfb1f95ef27ec23ef5c37fccc7da4e6fdc5327ad
Hager <hagerelewa50@gmail.com>
2024-03-20 00:00:29 +0200

    v1

--- HRService/obj/Debug/net8.0/HRService.AssemblyInfo.cs
+++ HRService/obj/Debug/net8.0/HRService.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbcc7cec21344a253b46fe21594b650dfb6f88e2")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633135ace7adb578a5da30176d4decd11390d0c9")]
->>>>>>> 12178f0cfd0978b95d0521b6e5c7aeb6334bef35
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdee7bfdc13d89ed589fa733b8cf3f4d8e806312")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
