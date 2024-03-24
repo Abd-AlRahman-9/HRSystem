f1f841e76b335c5e0721909d737433417a8d1bbe
Abd-AlRahman-9 <eldesoqe@outlook.com>
2024-03-24 12:07:46 +0200

    test

--- HRSystem/obj/Debug/net8.0/HRSystem.AssemblyInfo.cs
+++ HRSystem/obj/Debug/net8.0/HRSystem.AssemblyInfo.cs
@@ -14,15 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66158759051a83b6a1367ff9dc227c516b59c044")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ebcc3dd74245a493d525a2c2769f1583ce09bb")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef45f23b92b7fd272550bce923bafd5b8b54401f")]
->>>>>>> 66158759051a83b6a1367ff9dc227c516b59c044
->>>>>>> 04dce2d01a72ce67b8057adbeff559a802599627
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750305b85fca64b429b8b4e6f97300982c249612")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
