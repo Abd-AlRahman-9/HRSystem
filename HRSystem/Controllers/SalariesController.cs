f97eb444554126b96f991d1cc1b0139850b4838e
Abd-AlRahman-9 <eldesoqe@outlook.com>
2024-03-24 14:15:42 +0200

    Department parameter is done

--- HRSystem/Controllers/SalariesController.cs
+++ HRSystem/Controllers/SalariesController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HRDomain.Entities.DrivenEntities;
+using HRDomain.Specification.EntitiesSpecification;
 using HRDomain.Specification.Params;
 
 namespace HRSystem.Controllers
@@ -20,16 +21,11 @@ namespace HRSystem.Controllers
         [HttpGet]
         public async Task<ActionResult<Pagination<SalaryObj>>> Get([FromQuery]SalariesParams P)
         {
-<<<<<<< HEAD
             if (P.Year != 2024 || P.StartMonth > 03) return NotFound(new StatusResponse(404, $"Uneable to find salaries at {P.StartMonth}/{P.Year}"));
             if(P.StartMonth > P.EndMonth) return BadRequest(new StatusResponse(400, $"End date can't be before start date!"));
             var Data =  _ADOProcedures.GetSalaries(P);
             if(!Data.Any()) return BadRequest(new StatusResponse(404));
             return Ok(Data);
-=======
-            var Data = _ADOProcedures.GetSalaries(P);
-            return Ok(new Pagination<SalaryObj>(P.PageIndex,P.PageSize, _ADOProcedures.SalariesCount, Data));
->>>>>>> 5725e49831f347ba592d02be6c4a7e2a535e111d
         }
 
         [HttpGet("department")]
